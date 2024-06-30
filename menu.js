f3625e57cf3aeb6595e63fefe04aca9700d839a6
Oleg1224 <olegik.kr@gmail.com>
2024-06-30 14:00:03 +0300

    1

--- menu.js
+++ menu.js
@@ -5,6 +5,7 @@ headerMenu.addEventListener('click', function() {
     if(menu.className==='menu') {
         menu.classList.add('new')
         headerMenu.classList.add('rotate')
+        headerMenu.src='./img/pngwing.comыаыаыва.png'
         /* headerMenu.src = "./img/pngwing.com (13).png" */
         /* headerMenu.style.transform = "rotate(360deg)"; */
         /* menu.style.zIndex = '90' */
@@ -12,6 +13,7 @@ headerMenu.addEventListener('click', function() {
     else {
         menu.classList.remove('new')
         headerMenu.classList.remove('rotate')
+        headerMenu.src='./img/pngwing.com (13).png'
         /* headerMenu.src = './img/pngwing.comыаыаыва' */
         /* menu.style.zIndex = '80' */
     }
