f65517da6388a7873c63eecdce202f5a8aa384c8
Oleg1224 <olegik.kr@gmail.com>
2024-07-03 12:43:19 +0300

    1

--- menu.js
+++ menu.js
@@ -1,11 +1,13 @@
 let headerMenu = document.querySelector('.header__menu');
 let allMenu = document.querySelector('.all__menu');
 let menu = document.querySelector('.menu');
+let menuBox = document.querySelector('.menu__box');
 headerMenu.addEventListener('click', function() {
     if(menu.className==='menu') {
         menu.classList.add('new')
         headerMenu.classList.add('rotate')
         headerMenu.src='./img/pngwing.comыаыаыва.png'
+        menuBox.classList.add('top')
         /* headerMenu.src = "./img/pngwing.com (13).png" */
         /* headerMenu.style.transform = "rotate(360deg)"; */
         /* menu.style.zIndex = '90' */
@@ -14,6 +16,7 @@ headerMenu.addEventListener('click', function() {
         menu.classList.remove('new')
         headerMenu.classList.remove('rotate')
         headerMenu.src='./img/pngwing.com (13).png'
+        menuBox.classList.remove('top')
         /* headerMenu.src = './img/pngwing.comыаыаыва' */
         /* menu.style.zIndex = '80' */
     }
