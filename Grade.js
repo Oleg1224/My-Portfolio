f3ae95b2e28815ec5b6352c3e81b149a0f70d5e5
Oleg1224 <olegik.kr@gmail.com>
2024-07-13 20:03:27 +0300

    1

--- Grade.js
+++ Grade.js
@@ -1,9 +1,55 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let StarImg = document.querySelectorAll('.star__img');
-StarImg.addEventListener("mouseover", function() {
-    StarImg.src = "./img/Star.png";
-});
-StarImg.addEventListener("mouseout", function() {
-    StarImg.src = "./img/Star.png";
-});
+let StarImg1 = document.querySelector('.star__img1');
+let StarImg2 = document.querySelector('.star__img2');
+let StarImg3 = document.querySelector('.star__img3');
+let StarImg4 = document.querySelector('.star__img4');
+let StarImg5 = document.querySelector('.star__img5');
+StarImg1.addEventListener('mouseover', function() {
+    StarImg1.src="./img/Star.png"
+})
+StarImg1.addEventListener('mouseout', function() {
+    StarImg1.src="./img/StarNotFull.png"
+})
+StarImg2.addEventListener('mouseover', function() {
+    StarImg1.src="./img/Star.png"
+    StarImg2.src="./img/Star.png"
+})
+StarImg2.addEventListener('mouseout', function() {
+    StarImg1.src="./img/StarNotFull.png"
+    StarImg2.src="./img/StarNotFull.png"
+})
+StarImg3.addEventListener('mouseover', function() {
+    StarImg1.src="./img/Star.png"
+    StarImg2.src="./img/Star.png"
+    StarImg3.src="./img/Star.png"
+})
+StarImg3.addEventListener('mouseout', function() {
+    StarImg1.src="./img/StarNotFull.png"
+    StarImg2.src="./img/StarNotFull.png"
+    StarImg3.src="./img/StarNotFull.png"
+})
+StarImg4.addEventListener('mouseover', function() {
+    StarImg1.src="./img/Star.png"
+    StarImg2.src="./img/Star.png"
+    StarImg3.src="./img/Star.png"
+    StarImg4.src="./img/Star.png"
+})
+StarImg4.addEventListener('mouseout', function() {
+    StarImg1.src="./img/StarNotFull.png"
+    StarImg2.src="./img/StarNotFull.png"
+    StarImg3.src="./img/StarNotFull.png"
+    StarImg4.src="./img/StarNotFull.png"
+})
+StarImg5.addEventListener('mouseover', function() {
+    StarImg1.src="./img/Star.png"
+    StarImg2.src="./img/Star.png"
+    StarImg3.src="./img/Star.png"
+    StarImg4.src="./img/Star.png"
+    StarImg5.src="./img/Star.png"
+})
+StarImg5.addEventListener('mouseout', function() {
+    StarImg1.src="./img/StarNotFull.png"
+    StarImg2.src="./img/StarNotFull.png"
+    StarImg3.src="./img/StarNotFull.png"
+    StarImg4.src="./img/StarNotFull.png"
+    StarImg5.src="./img/StarNotFull.png"
 })
