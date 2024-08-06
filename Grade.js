4a84c00ff8f8d09444459fa7a061e696a1e7acc9
Oleg1224 <olegik.kr@gmail.com>
2024-08-06 22:48:32 +0300

    1

--- Grade.js
+++ Grade.js
@@ -5,39 +5,59 @@ let StarImg4 = document.querySelector('.star__img4');
 let StarImg5 = document.querySelector('.star__img5');
 StarImg1.addEventListener('mouseover', function() {
     StarImg1.src="./img/Star.png"
+    StarImg1.style.transform='rotate(3deg) scale(1.2)'
 })
 StarImg1.addEventListener('mouseout', function() {
     StarImg1.src="./img/StarNotFull.png"
+    StarImg1.style.transform='rotate(0deg)'
 })
 StarImg2.addEventListener('mouseover', function() {
     StarImg1.src="./img/Star.png"
     StarImg2.src="./img/Star.png"
+    StarImg1.style.transform='rotate(3deg) scale(1.2)'
+    StarImg2.style.transform='rotate(3deg) scale(1.2)'
 })
 StarImg2.addEventListener('mouseout', function() {
     StarImg1.src="./img/StarNotFull.png"
     StarImg2.src="./img/StarNotFull.png"
+    StarImg1.style.transform='rotate(0deg)'
+    StarImg2.style.transform='rotate(0deg)'
 })
 StarImg3.addEventListener('mouseover', function() {
     StarImg1.src="./img/Star.png"
     StarImg2.src="./img/Star.png"
     StarImg3.src="./img/Star.png"
+    StarImg1.style.transform='rotate(3deg) scale(1.2)'
+    StarImg2.style.transform='rotate(3deg) scale(1.2)'
+    StarImg3.style.transform='rotate(3deg) scale(1.2)'
 })
 StarImg3.addEventListener('mouseout', function() {
     StarImg1.src="./img/StarNotFull.png"
     StarImg2.src="./img/StarNotFull.png"
     StarImg3.src="./img/StarNotFull.png"
+    StarImg1.style.transform='rotate(0deg)'
+    StarImg2.style.transform='rotate(0deg)'
+    StarImg3.style.transform='rotate(0deg)'
 })
 StarImg4.addEventListener('mouseover', function() {
     StarImg1.src="./img/Star.png"
     StarImg2.src="./img/Star.png"
     StarImg3.src="./img/Star.png"
     StarImg4.src="./img/Star.png"
+    StarImg1.style.transform='rotate(3deg) scale(1.2)'
+    StarImg2.style.transform='rotate(3deg) scale(1.2)'
+    StarImg3.style.transform='rotate(3deg) scale(1.2)'
+    StarImg4.style.transform='rotate(3deg) scale(1.2)'
 })
 StarImg4.addEventListener('mouseout', function() {
     StarImg1.src="./img/StarNotFull.png"
     StarImg2.src="./img/StarNotFull.png"
     StarImg3.src="./img/StarNotFull.png"
     StarImg4.src="./img/StarNotFull.png"
+    StarImg1.style.transform='rotate(0deg)'
+    StarImg2.style.transform='rotate(0deg)'
+    StarImg3.style.transform='rotate(0deg)'
+    StarImg4.style.transform='rotate(0deg)'
 })
 StarImg5.addEventListener('mouseover', function() {
     StarImg1.src="./img/Star.png"
@@ -45,6 +65,11 @@ StarImg5.addEventListener('mouseover', function() {
     StarImg3.src="./img/Star.png"
     StarImg4.src="./img/Star.png"
     StarImg5.src="./img/Star.png"
+    StarImg1.style.transform='rotate(3deg) scale(1.2)'
+    StarImg2.style.transform='rotate(3deg) scale(1.2)'
+    StarImg3.style.transform='rotate(3deg) scale(1.2)'
+    StarImg4.style.transform='rotate(3deg) scale(1.2)'
+    StarImg5.style.transform='rotate(3deg) scale(1.2)'
 })
 StarImg5.addEventListener('mouseout', function() {
     StarImg1.src="./img/StarNotFull.png"
@@ -52,6 +77,10 @@ StarImg5.addEventListener('mouseout', function() {
     StarImg3.src="./img/StarNotFull.png"
     StarImg4.src="./img/StarNotFull.png"
     StarImg5.src="./img/StarNotFull.png"
+    StarImg1.style.transform='rotate(0deg)'
+    StarImg2.style.transform='rotate(0deg)'
+    StarImg3.style.transform='rotate(0deg)'
+    StarImg4.style.transform='rotate(0deg)'
 })
 
 
