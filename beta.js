62fc249a1277d30b1158923ba90c0387f5a44e68
Oleg1224 <olegik.kr@gmail.com>
2024-08-27 18:34:28 +0300

    1

--- beta.js
+++ beta.js
@@ -1,8 +1,8 @@
 window.onload = function() {
     let betaBgraund = document.querySelector('.beta_bgraund');
-    setTimeout(function() {
+    /* setTimeout(function() {
         betaBgraund.style.transform='scale(0)'
-    }, 20000)
+    }, 20000) */
 }
 let betaButton = document.querySelector('.beta_button');
 betaButton.addEventListener('click', function() {
